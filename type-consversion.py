8615ca2c1372ae2f9d81da3b3f485183fd0067d6
hamzahayri <hamzahayriburan@gmail.com>
2026-08-12 22:47:31 +0300

    Implement type conversion and input handling
    Added type conversion examples and user input handling.

---
+++ type-consversion.py
@@ -0,0 +1,53 @@
+'''
+x = input('1 sayi: ')
+y = input('2 sayi: ')
+
+print(type(x))
+print(type(y))
+
+toplam = int(x) + int(y)
+
+print(toplam)
+'''
+
+x = 5         #int
+y = 2.5       #float
+name = 'Hamza' #str
+isOnline = True  #bool
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+# Type Conversion
+
+#int to float
+
+# x = float(x)
+# print(x)
+# print(type(x))
+
+# float to int
+
+# y = int(y)
+# print(y)
+# print(type(y))
+
+# result = str(x) + str(y)
+# print(result)
+# print(type(result))
+
+# bool to str
+
+# isOnline = str(isOnline)
+# print(isOnline)
+# print(type(isOnline))
+
+# bool to int
+
+isOnline = False # False oldugunda 0, True oldugunda 1 olarak doner
+
+# isOnline = int(isOnline)
+# print(isOnline)
+# print(type(isOnline))
