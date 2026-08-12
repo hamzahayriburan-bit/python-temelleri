720a39e80c495dbdf30b603bd1a8f7357a06de71
hamzahayri <hamzahayriburan@gmail.com>
2026-08-12 18:20:35 +0300

    Add customer and order details in variables-demo.py

---
+++ variables-demo.py
@@ -0,0 +1,31 @@
+musteri_adi = "Mete"
+musteri_soyadi = "Kara"
+musteri_cinsiyet = True # Erkek
+musteri_tc_kimlik = "12345678910"
+musteri_dogum_tarihi = "01.01.2000"
+musteri_adres = "İstanbul"
+musteri_yas = 24
+
+print(musteri_adi + " " + musteri_soyadi)
+print(musteri_cinsiyet)
+print(musteri_tc_kimlik)
+print(musteri_dogum_tarihi)
+print(musteri_adres)
+print(musteri_yas)
+
+'''''
+Siparis 1 => 110 TL
+Siparis 2 => 1100.05 TL
+Siparis 3 => 365.95 TL
+
+'''''
+
+
+order1 = 110
+order2 = 1100.05
+order3 = 365.95
+
+total_order = order1 + order2 + order3
+
+print("Total", total_order,)
+
